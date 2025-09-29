f4b4f2b26c45f3dfc9625cdb4d4a2ae2406a35d7
harmansandhu-glitch <harmanjotsingh200310@gmail.com>
2025-09-29 23:44:22 +0530

    sixth commit

--- services/document-service/src/main/java/com/project/pms/documentservice/repository/DocumentRepository.java
+++ services/document-service/src/main/java/com/project/pms/documentservice/repository/DocumentRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentRepository extends MongoRepository<Document, String> {
-}
+public interface DocumentRepository extends MongoRepository<Document, String> {};
