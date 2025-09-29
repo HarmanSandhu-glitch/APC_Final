22dd3f027e96620dc1edde90fd2867bcf5ad2c60
harmansandhu-glitch <harmanjotsingh200310@gmail.com>
2025-09-29 23:48:25 +0530

    commit

--- services/auth-service/src/test/java/com/project/pms/authservice/AuthServiceApplication.java
+++ services/auth-service/src/test/java/com/project/pms/authservice/AuthServiceApplication.java
@@ -1,17 +1,15 @@
-package com.project.pms.leaveservice;
+package com.project.pms.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class LeaveServiceApplication {
+public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LeaveServiceApplication.class, args);
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 
 }
